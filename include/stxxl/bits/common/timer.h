775c9bac645b14cf07bebc21654cba42e18747a5
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-04-10 10:32:06 +0000

    merge timestamp unification (from trunk r510, r520, r563)
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/branches/parallel_pipelining@712 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/common/timer.h
+++ include/stxxl/bits/common/timer.h
@@ -6,19 +6,46 @@
 
 #include "stxxl/bits/namespace.h"
 
-#include <time.h>
-
-#ifndef BOOST_MSVC
- #include <sys/time.h>
+#ifdef BOOST_MSVC
+ // no alternative to boost :-(
+ #define STXXL_NONMONOTONIC_BOOST_TIMESTAMP
 #endif
 
-#ifdef STXXL_BOOST_TIMESTAMP
+#if defined(STXXL_BOOST_TIMESTAMP) && \
+    defined(STXXL_NONMONOTONIC_BOOST_TIMESTAMP)
  #include <boost/date_time/posix_time/posix_time.hpp>
  #include <cmath>
+#else
+ #include <ctime>
+ #include <sys/time.h>
 #endif
 
 
 __STXXL_BEGIN_NAMESPACE
+
+//! \brief Returns number of seconds since the epoch (or midnight, if boost timer is used), high resolution.
+inline double
+timestamp ()
+{
+#ifdef STXXL_NONMONOTONIC_BOOST_TIMESTAMP
+    boost::posix_time::ptime MyTime = boost::posix_time::microsec_clock::local_time();
+    boost::posix_time::time_duration Duration = MyTime.time_of_day();
+#ifdef BOOST_MSVC
+#pragma message("FIXME: stxxl_timestamp() is non-monotonic, boost::posix_time::microsec_clock::local_time().time_of_day() resets on midnight")
+#else
+#warning FIXME: stxxl_timestamp() is non-monotonic, boost::posix_time::microsec_clock::local_time().time_of_day() resets on midnight
+#endif
+    double sec = double (Duration.hours()) * 3600. +
+                 double (Duration.minutes()) * 60. +
+                 double (Duration.seconds()) +
+                 double (Duration.fractional_seconds()) / (pow(10., Duration.num_fractional_digits()));
+    return sec;
+#else
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return double (tp.tv_sec) + tp.tv_usec / 1000000.;
+#endif
+}
 
 class timer
 {
@@ -41,19 +68,7 @@ timer::timer() : running(false), accumulated(0.)
 
 double timer::timestamp()
 {
-#ifdef STXXL_BOOST_TIMESTAMP
-    boost::posix_time::ptime MyTime = boost::posix_time::microsec_clock::local_time();
-    boost::posix_time::time_duration Duration = MyTime.time_of_day();
-    double sec = double (Duration.hours()) * 3600. +
-                 double (Duration.minutes()) * 60. +
-                 double (Duration.seconds()) +
-                 double (Duration.fractional_seconds()) / (pow(10., Duration.num_fractional_digits()));
-    return sec;
-#else
-    struct timeval tp;
-    gettimeofday(&tp, NULL);
-    return double (tp.tv_sec) + tp.tv_usec / 1000000.;
-#endif
+   return stxxl::timestamp();
 }
 
 void timer::start()
@@ -61,11 +76,13 @@ void timer::start()
     running = true;
     last_clock = timestamp();
 }
+
 void timer::stop()
 {
     running = false;
     accumulated += timestamp() - last_clock;
 }
+
 void timer::reset()
 {
     accumulated = 0.;
@@ -77,7 +94,6 @@ double timer::mseconds()
     if (running)
         return (accumulated + timestamp() - last_clock) * 1000.;
 
-
     return (accumulated * 1000.);
 }
 
@@ -86,7 +102,6 @@ double timer::useconds()
     if (running)
         return (accumulated + timestamp() - last_clock) * 1000000.;
 
-
     return (accumulated * 1000000.);
 }
 
@@ -94,7 +109,6 @@ double timer::seconds()
 {
     if (running)
         return (accumulated + timestamp() - last_clock);
-
 
     return (accumulated);
 }
