bb11b72a4e09a86ecd13eaa55c5b86409c0b8ada
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2009-10-15 12:42:35 +0000

    get LBA addresses (relative to partition start) for the discard ranges
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/branches/trim_file@2041 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- io/trim_file.cpp
+++ io/trim_file.cpp
@@ -14,7 +14,18 @@
 
 #if STXXL_HAVE_TRIM_FILE
 
+#include <linux/fs.h>
+#include <linux/fiemap.h>
+#include <sys/ioctl.h>
+
 __STXXL_BEGIN_NAMESPACE
+
+
+#define NUMEXTENTS 16
+struct fiemap_request {
+    struct fiemap fm;
+    struct fiemap_extent fm_extents[NUMEXTENTS];
+};
 
 
 trim_file::trim_file(
@@ -27,6 +38,49 @@ trim_file::trim_file(
 void trim_file::discard(offset_type offset, offset_type size)
 {
     STXXL_VERBOSE("trim_file::discard(0x" << std::hex << offset << ", 0x" << size << ")");
+    // get LBA blocks for the requested range
+    offset_type start = offset;
+    offset_type end = start + size;
+    while (start < end) {
+        fiemap_request fs;
+        assert(&fs.fm_extents[0] == &fs.fm.fm_extents[0]);
+        memset(&fs, 0, sizeof(fiemap_request));
+        fs.fm.fm_start = start;
+        fs.fm.fm_length = end - start;
+        fs.fm.fm_flags = 0;
+        fs.fm.fm_extent_count = NUMEXTENTS;
+        int err;
+        {
+            scoped_mutex_lock(this->fd_mutex);
+            err = ioctl(file_des, FS_IOC_FIEMAP, &fs);
+        }
+        if (err < 0) {
+            err = errno;
+            STXXL_ERRMSG("ioctl(FIEMAP) failed (" << err << "): " << strerror(err));
+            return;
+        }
+        STXXL_VERBOSE0("FIEMAP 0x" << std::hex << fs.fm.fm_start << " 0x" << fs.fm.fm_length << " => " << fs.fm.fm_mapped_extents);
+        if (!fs.fm.fm_mapped_extents)
+            break;
+        for (unsigned i = 0; i < fs.fm.fm_mapped_extents; ++i) {
+            STXXL_VERBOSE0("extent " << i << "/" << fs.fm.fm_mapped_extents << "  logical=0x" << std::hex << fs.fm_extents[i].fe_logical << "  physical=0x" << fs.fm_extents[i].fe_physical
+                           << "  length=0x" << fs.fm_extents[i].fe_length << "  flags=0x" << fs.fm_extents[i].fe_flags);
+            start = fs.fm_extents[i].fe_logical + fs.fm_extents[i].fe_length;
+            if (fs.fm_extents[i].fe_flags & (FIEMAP_EXTENT_UNKNOWN | FIEMAP_EXTENT_ENCODED | FIEMAP_EXTENT_NOT_ALIGNED))
+                continue;
+            offset_type phy_start = fs.fm_extents[i].fe_physical;
+            if (offset > fs.fm_extents[i].fe_logical)
+                phy_start += offset - fs.fm_extents[i].fe_logical;
+            offset_type phy_end = fs.fm_extents[i].fe_physical + fs.fm_extents[i].fe_length;
+            if (fs.fm_extents[i].fe_logical + fs.fm_extents[i].fe_length > end)
+                phy_end -= fs.fm_extents[i].fe_logical + fs.fm_extents[i].fe_length - end;
+            STXXL_VERBOSE0("physical location: 0x" << std::hex << phy_start << "  length: 0x" << phy_end - phy_start);
+            if (fs.fm_extents[i].fe_flags & FIEMAP_EXTENT_LAST) {
+                start = end;
+                break;
+            }
+        }
+    }
 }
 
 const char * trim_file::io_type() const
